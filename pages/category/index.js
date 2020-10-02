248b40e53767bd8eac5ceeab780b5025038a4b3f
TomatoZ7 <17612064467@163.com>
2020-10-02 13:17:11 +0800

    分类完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,6 +1,7 @@
 // pages/category/index.js
 // 引入 发送请求的方法
-import { request } from "../../request/index.js"
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
@@ -12,29 +13,77 @@ Page({
     // 右侧商品数据
     rightContent: [],
     // 左侧菜单点击
-    currentIndex: 0
+    currentIndex: 0,
+    scrollTop: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getCates();
+    // 获取本地存储数据并判断
+    const cates = wx.getStorageSync('cates');
+    if(!cates){
+      this.getCates();
+    }else{
+      if(Date.now()-cates.time>1000*60*30){
+        // 数据过期
+        this.getCates();
+      }else{
+        this.cates = cates.data;
+        // 构造左侧滚动栏数据
+        let leftMenuList = this.cates.map(v=>v.cat_name);
+        // 构造右侧商品栏数据
+        let rightContent = this.cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent
+        });
+      }
+    }
   },
   
   // 获取左侧分类栏数据
-  getCates(){
-    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/categories"})
-    .then(res=>{
-      this.cates = res.data.message;
-      // 构造左侧滚动栏数据
-      let leftMenuList = this.cates.map(v=>v.cat_name);
-      // 构造右侧商品栏数据
-      let rightContent = this.cates[0].children;
-      this.setData({
-        leftMenuList,
-        rightContent
-      });
-    })
+  async getCates(){
+    // request({url:"/categories"})
+    // .then(res=>{
+      // this.cates = res.data.message;
+      // // 数据缓存本地
+      // wx.setStorageSync('cates', {'time':Date.now(), data:this.cates});
+      // // 构造左侧滚动栏数据
+      // let leftMenuList = this.cates.map(v=>v.cat_name);
+      // // 构造右侧商品栏数据
+      // let rightContent = this.cates[0].children;
+      // this.setData({
+      //   leftMenuList,
+      //   rightContent
+      // });
+    // })
+
+    // es7
+    const res = await request({url:"/categories"});
+    this.cates = res;
+    // 数据缓存本地
+    wx.setStorageSync('cates', {'time':Date.now(), data:this.cates});
+    // 构造左侧滚动栏数据
+    let leftMenuList = this.cates.map(v=>v.cat_name);
+    // 构造右侧商品栏数据
+    let rightContent = this.cates[0].children;
+    this.setData({
+      leftMenuList,
+      rightContent
+    });
   },
+
+  // 左侧菜单的点击事件
+  handleItemTap(e){
+    const {index} = e.currentTarget.dataset;
+    // 构造右侧商品栏数据
+    let rightContent = this.cates[index].children;
+    this.setData({
+      currentIndex: index,
+      rightContent,
+      scrollTop:0
+    });
+  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -19,6 +19,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // Q:这里是同时发送请求，互不影响吗？
     this.getSwiperList();
     this.getCateList();
     this.getFloorList();
@@ -26,30 +27,30 @@ Page({
 
   // 获取轮播图数据
   getSwiperList(){
-    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"})
+    request({url:"/home/swiperdata"})
     .then(result=>{
       this.setData({
-        swiperList: result.data.message
+        swiperList: result
       })
     })
   },
 
   // 获取导航栏数据
   getCateList(){
-    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"})
+    request({url:"/home/catitems"})
     .then(result=>{
       this.setData({
-        catesList: result.data.message
+        catesList: result
       })
     })
   },
 
   // 获取楼层数据
   getFloorList(){
-    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/floordata"})
+    request({url:"/home/floordata"})
     .then(result=>{
       this.setData({
-        floorList: result.data.message
+        floorList: result
       })
     })
   }
